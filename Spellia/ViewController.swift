19ae70ad4b324c72a9a5aa7c85e202b2abb7cfc8
leticiadutra22-23 <leticiack.ldtra@gmail.com>
2022-10-11 10:07:19 -0300

    Initial Commit

---
+++ Spellia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spellia
+//
+//  Created by Maria Letícia Dutra de Oliveira on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SpelliaUITests/SpelliaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SpelliaUITestsLaunchTests.swift
+//  SpelliaUITests
+//
+//  Created by Maria Letícia Dutra de Oliveira on 11/10/22.
+//
+
+import XCTest
+
+final class SpelliaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
