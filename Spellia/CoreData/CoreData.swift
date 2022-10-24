49b81db44fe3d8b6f83703507a894961fd56588b
leticiadutra22-23 <leticiack.ldtra@gmail.com>
2022-10-24 08:59:32 -0300

    need to do core data to save favorites

--- Spellia/CoreData/CoreData.swift
+++ Spellia/CoreData/CoreData.swift
@@ -8,47 +8,47 @@
 import UIKit
 import CoreData
 
-class CoreData {
-
-    static let sharedInstance = CoreData()
-    private init(){}
-
-    private let container: NSPersistentContainer? = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
-
-    func saveDataOf(spell:[Spell]){
-        self.container?.performBackgroundTask{ [weak self] (context) in
-            self?.deleteObjectsfromCoreData(context: context)
-            self?.saveDataToCoreData(favitem: spell, context: context)
-        }
-    }
-
-    private let fetchRequest = NSFetchRequest<FavoriteItems>(entityName: "FavoriteItems")
-
-    private func deleteObjectsfromCoreData(context: NSManagedObjectContext) {
-        do {
-            let objects = try context.fetch(fetchRequest)
-            _ = objects.map({context.delete($0)})
-
-            try context.save()
-        } catch {
-            print("Deleting Error: \(error)")
-        }
-    }
-
-    private func saveDataToCoreData(favitem:[Spell], context: NSManagedObjectContext) {
-        context.perform {
-            print("Are we on the main Queue?  \(String(describing: Thread.isMainThread))")
-            for favr in favitem {
-                let favSpell = FavoriteItems(context: context)
-                favSpell.name = favr.name
-                favSpell.type = favr.type
-            }
-
-            do {
-                try context.save()
-            } catch {
-                fatalError("Failure to save context: \(error)")
-            }
-        }
-    }
-}
+//class CoreData {
+//
+//    static let sharedInstance = CoreData()
+//    private init(){}
+//
+//    private let container: NSPersistentContainer? = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
+//
+//    func saveDataOf(spell:[Spell]){
+//        self.container?.performBackgroundTask{ [weak self] (context) in
+//            self?.deleteObjectsfromCoreData(context: context)
+//            self?.saveDataToCoreData(favitem: spell, context: context)
+//        }
+//    }
+//
+//    private let fetchRequest = NSFetchRequest<FavoriteItems>(entityName: "FavoriteItems")
+//
+//    private func deleteObjectsfromCoreData(context: NSManagedObjectContext) {
+//        do {
+//            let objects = try context.fetch(fetchRequest)
+//            _ = objects.map({context.delete($0)})
+//
+//            try context.save()
+//        } catch {
+//            print("Deleting Error: \(error)")
+//        }
+//    }
+//
+//    private func saveDataToCoreData(favitem:[Spell], context: NSManagedObjectContext) {
+//        context.perform {
+//            print("Are we on the main Queue?  \(String(describing: Thread.isMainThread))")
+//            for favr in favitem {
+//                let favSpell = FavoriteItems(context: context)
+//                favSpell.name = favr.name
+//                favSpell.type = favr.type
+//            }
+//
+//            do {
+//                try context.save()
+//            } catch {
+//                fatalError("Failure to save context: \(error)")
+//            }
+//        }
+//    }
+//}
